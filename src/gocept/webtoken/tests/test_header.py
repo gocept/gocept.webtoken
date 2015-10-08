c6f839843c19dadab74cca670b5024bea4f0147d
Michael Howitz <mh@gocept.com>
2015-10-08 10:00:58 +0200

    PEP-257

--- src/gocept/webtoken/tests/test_header.py
+++ src/gocept/webtoken/tests/test_header.py
@@ -3,34 +3,34 @@ import pytest
 
 
 def test_header__create_authorization_header__1():
-    """Creates bearer header for a given webtoken dict."""
+    """Function creates bearer header for a given webtoken dict."""
     key, value = create_authorization_header({'token': b'<TOKEN>'})
     assert 'Authorization' == key
     assert 'Bearer <TOKEN>' == value
 
 
 def test_header__create_authorization_header__2():
-    """Creates bearer header for a given webtoken."""
+    """Function creates bearer header for a given webtoken."""
     key, value = create_authorization_header(b'<TOKEN>')
     assert 'Authorization' == key
     assert 'Bearer <TOKEN>' == value
 
 
 def test_header__extract_token__1():
-    """Extracts token from given dict."""
+    """`extract_token()` extracts token from given dict."""
     headers = dict(Authorization='Bearer <TOKEN>')
     assert b'<TOKEN>' == extract_token(headers)
 
 
 def test_header__extract_token__2():
-    """Raises ValueError if Authorization header is missing."""
+    """`extract_token()`  raises ValueError if Authorization key is missing."""
     with pytest.raises(ValueError) as err:
         extract_token({})
     assert 'Missing Authorization header' == str(err.value)
 
 
 def test_header__extract_token__3():
-    """Raises ValueError if Authorization scheme is not Bearer."""
+    """`extract_token()`  raises ValueError on wrong Authorization scheme."""
     headers = dict(Authorization='Foobar <TOKEN>')
     with pytest.raises(ValueError) as err:
         extract_token(headers)
@@ -38,7 +38,7 @@ def test_header__extract_token__3():
 
 
 def test_header__extract_token__4():
-    """Raises ValueError if scheme is missing."""
+    """`extract_token()`  raises ValueError if scheme is missing."""
     headers = dict(Authorization='<TOKEN>')
     with pytest.raises(ValueError) as err:
         extract_token(headers)
