4c3c7af1790f5b8667bc8df11c604bce306e5009
Steffen Allner <sa@gocept.com>
2016-03-21 17:37:51 +0100

    Flake8 fixes.

--- src/gocept/webtoken/__init__.py
+++ src/gocept/webtoken/__init__.py
@@ -1,4 +1,4 @@
-from .interfaces import ICryptographicKeys
-from .keys import CryptographicKeys
-from .token import create_web_token, decode_web_token
-from .header import create_authorization_header, extract_token
+from .interfaces import ICryptographicKeys  # NOQA
+from .keys import CryptographicKeys  # NOQA
+from .token import create_web_token, decode_web_token  # NOQA
+from .header import create_authorization_header, extract_token  # NOQA
