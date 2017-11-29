3d81dc5b0d4c1e6918ced5a9ace60494a2cae9e6
Steffen Allner <sa@gocept.com>
2017-11-29 10:57:13 +0100

    Drop support for Python 3.3 but add it for 3.6.

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@
 
 from setuptools import setup, find_packages
 import glob
-import os.path
 
 
 setup(
     name='gocept.webtoken',
-    version='1.3.dev0',
+    version='2.0.dev0',
 
     install_requires=[
         'cryptography',
@@ -41,9 +40,9 @@ Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Programming Language :: Python :: Implementation
 Programming Language :: Python :: Implementation :: CPython
 """[:-1].split('\n'),
